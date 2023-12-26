34819ebcb9ceefe2a78756abd83c016c5baea21f
Matias Tolppanen <matias.tolppanen@outlook.com>
2023-12-26 16:05:00 +0200

    feat: register user

--- src/app.py
+++ src/app.py
@@ -10,6 +10,25 @@ def index():
     return 'Hello, World!'
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('blog.db')
+        cursor = conn.cursor()
+
+        # Intentionally vulnerable to A02: Store passwords in plain text
+        cursor.execute(
+            f"INSERT INTO users (username, password) VALUES ('{username}', '{password}')")
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
 # Security Misconfiguration (A05): Leave debug mode on in production, exposing sensitive information.
 if __name__ == '__main__':
     app.run(debug=True)
