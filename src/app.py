d0c1ec31de65a37c08aedf0da159bcc3ea442b88
Matias Tolppanen <matias.tolppanen@outlook.com>
2023-12-26 16:09:57 +0200

    feat: basic login

--- src/app.py
+++ src/app.py
@@ -29,6 +29,30 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('blog.db')
+        cursor = conn.cursor()
+
+        # Intentionally vulnerable to A03: Using string concatenation in SQL query
+        cursor.execute(
+            f"SELECT id FROM users WHERE username = '{username}' AND password = '{password}'")
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session['user_id'] = user[0]
+            return redirect(url_for('index'))
+        else:
+            return 'Login Failed'
+
+    return render_template('login.html')
+
+
 # Security Misconfiguration (A05): Leave debug mode on in production, exposing sensitive information.
 if __name__ == '__main__':
     app.run(debug=True)
